927782fb06f63f60cb1da163b548a21f82ca99ec
kongjiale <kongjiale12@gmail.com>
2025-01-18 14:43:22 +0800

    Delete everything

--- admin/js/admin-index.js
+++
@@ -1,8 +0,0 @@
-//? Redirect To The Same Page when Reload To Clear The URL
-window.addEventListener("load", function () {
-  // Check if the navigation type is reload
-  if (performance.getEntriesByType("navigation")[0].type === "reload") {
-    // Redirect to another page
-    window.location.href = "admin-index.php"; // Replace with the target URL
-  }
-});
